3146b7de13760db010d6ea0f4892dcd5fbbbd852
Karol <pandamax.karol.lichon@gmail.com>
2023-10-01 19:27:19 +0200

    added effect and song

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-let board, boardWidth, boardHeight, context, planeWidth, planeHeight, planeX, planeY, planeImg, plane, pipeWidth, pipeHeight, pipeX, pipeY, topPipeImg,
-bottomPipeImg, velocityX, velocityY, gravity, gameOver, score
+let board, boardWidth, boardHeight, context, planeWidth, planeHeight, planeX, planeY, planeImg, plane, towersWidth, towersHeight, towersX, towersY, toptowersImg, bottomtowersImg, velocityX, velocityY, gravity, gameOver, score, setVelocityY, spanScore, explosion, gameOverSpan, explosionSound, explosionSoundPeople
 
 const main = () => {
 	let btns = document.querySelectorAll('.btn')
     board = document.querySelector('.board')
+    spanScore = document.querySelector('.score')
+    explosion = document.querySelector('.explosion')
+    gameOverSpan = document.querySelector('.game-over')
     settings()
 
 	btns.forEach((el) => el.addEventListener('click', () => {
@@ -13,14 +15,15 @@ const main = () => {
 					;[btns[1], btns[2]].forEach((el) => (el.style.display = 'block'))
 				}, 200)
 			} else if (el.classList.contains('btn--easy')) {
-				console.log('easy')
+                setVelocityY = 0
                 startGame()
 			} else {
-				console.log('hard')
                 startGame()
+                setVelocityY = .6
 			}
 		})
 	)
+
 }
 
 const settings = () => {
@@ -39,9 +42,8 @@ const settings = () => {
 }
 
     //towers setting
-    // pipe zaminic na towers
-    pipeArray = []
-    pipeWidth = 80, pipeHeight = 412, pipeX = boardWidth, pipeY = 0
+    towersArray = []
+    towersWidth = 80, towersHeight = 412, towersX = boardWidth, towersY = 0
 
     //physic
     velocityX = -2
@@ -51,10 +53,15 @@ const settings = () => {
     gameOver = false
     score = 0
 
+    explosionSound = new Audio('./explosion-ground.wav')
+    explosionSoundPeople = new Audio('./explosion-people.wav')
+
 }
 
-
 const startGame = () => {
+    // window.onload = function () {
+    // setVelocityY = 0
+
     document.querySelector('.menu').style.display = 'none'
     board.width = boardWidth
     board.height = boardHeight
@@ -66,37 +73,41 @@ const startGame = () => {
         context.drawImage(planeImg, plane.x, plane.y, plane.width, plane.height)
     }
 
-    topPipeImg = new Image()
-    topPipeImg.src = 'img/topSkyscraper.png'
+    toptowersImg = new Image()
+    toptowersImg.src = 'img/topSkyscraper.png'
 
-    bottomPipeImg = new Image()
-    bottomPipeImg.src = 'img/bottomSkyscraper.png'
+    bottomtowersImg = new Image()
+    bottomtowersImg.src = 'img/bottomSkyscraper.png'
 
     requestAnimationFrame(update)
-    // placePipes()
-    setInterval(placePipes, 3500)
-    document.addEventListener('keydown', moveBrid)
-    document.addEventListener('click', moveBrid)
+    placetowerss()
+    setInterval(placetowerss, 3500)
+    board.addEventListener('keydown', movePlane)
+    board.addEventListener('click', movePlane)
 
-    document.addEventListener('mousedown', function() {
+    board.addEventListener('mousedown', () => {
+        gravity = 0
+        velocityY = setVelocityY
+    })
+    board.addEventListener('mouseup', () => {
+        gravity = 0.4
+    })
+
+    board.addEventListener('touchstart', () => {
         gravity = 0;
-        velocityY = .2
-    });
-
-    document.addEventListener('mouseup', function() {
-        gravity = 0.4;
-    });
-
-    document.addEventListener('touchstart', function(event) {
-        event.preventDefault(); 
-        gravity = 0;
-        velocityY = 0.2;
-    });
-    
-    document.addEventListener('touchend', function(e) {
-        gravity = 0.4;
+        velocityY = setVelocityY
+    })
+    board.addEventListener('touchend', () => {
+        gravity = 0.4
         velocityY = -6
-    });
+    })
+    gameOverSpan.addEventListener('click', e => {
+        movePlane(e)
+    })
+
+    explosion.addEventListener('click', e => {
+        movePlane(e)
+    })
 }
 
 function update() {
@@ -108,67 +119,89 @@ function update() {
     plane.y = Math.max(plane.y + velocityY, 0)
     context.drawImage(planeImg, plane.x, plane.y, plane.width, plane.height)
 
-    if (plane.y > board.height) gameOver = true
+    if (plane.y > board.height){
+        gameOver = true
+        explosion.style.display = 'block'
+        explosion.setAttribute('src', './img/fall-plane.gif')
+        gameOverSpan.style.display = 'block'
+        explosionSound.play() // usunąć
 
-    //.zamienisc na forecha lub cos
-    for (let i = 0; i < pipeArray.length; i++) {
-        let pipe = pipeArray[i]
-        pipe.x += velocityX
-        context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height)
+        setTimeout(() => {
+            explosion.style.display = 'none'
+        }, 3400);
+    }
 
-        if (!pipe.passed && plane.x > pipe.x + pipe.width) {
+    for (let i = 0; i < towersArray.length; i++) {
+        let towers = towersArray[i]
+        towers.x += velocityX
+        context.drawImage(towers.img, towers.x, towers.y, towers.width, towers.height)
+
+        if (!towers.passed && plane.x > towers.x + towers.width) {
             score += .5
-            pipe.passed = true
+            spanScore.textContent = score
+            towers.passed = true
         }
 
-        if(detectCollision(plane, pipe))gameOver = true
+        if(detectCollision(plane, towers)){
+            gameOver = true
+            explosion.setAttribute('src', './img/explosion.gif')
+            explosion.style.display = 'block'
+            explosionSoundPeople.play()
+
+            setTimeout(() => {
+                explosion.style.display = 'none'
+                gameOver ? gameOverSpan.style.display = 'block' : null
+            }, 600); 
+        }
     }
 
-    while (pipeArray.length > 0 && pipeArray[0].x < -pipeWidth) pipeArray.shift()
-
-    context.fillStyle = 'white'
-    context.font = '45px sans-serif'
-    context.fillText(score, 5, 45)
-
-    if (gameOver) context.fillText("GAME OVER", 5, 90)
+    while (towersArray.length > 0 && towersArray[0].x < -towersWidth) towersArray.shift()
 }
 
-function placePipes() {
+function placetowerss() {
     if (gameOver) return
-    let randomPipeY = pipeY - pipeHeight/4 - Math.random()*(pipeHeight/2)
+    let randomtowersY = towersY - towersHeight/4 - Math.random()*(towersHeight/2)
     let openingSpace = board.height/3
-
-    let topPipe = {
-        img: topPipeImg,
-        x: pipeX,
-        y: randomPipeY,
-        width: pipeWidth,
-        height: pipeHeight,
+    
+    let toptowers = {
+        img: toptowersImg,
+        x: towersX,
+        y: randomtowersY,
+        width: towersWidth,
+        height: towersHeight,
         passed: false
     }
-    pipeArray.push(topPipe)
+    towersArray.push(toptowers)
 
-    let bottomPipe = {
-        img: bottomPipeImg,
-        x: pipeX,
-        y: randomPipeY + pipeHeight + openingSpace,
-        width: pipeWidth,
-        height: pipeHeight,
+    let bottomtowers = {
+        img: bottomtowersImg,
+        x: towersX,
+        y: randomtowersY + towersHeight + openingSpace,
+        width: towersWidth,
+        height: towersHeight,
         passed: false
     }
-    pipeArray.push(bottomPipe)
+    towersArray.push(bottomtowers)
 }
 
-function moveBrid(e) {
+function movePlane(e) {
     if (e.type === 'click' || e.code === 'Space' || e.code === 'ArrowUp') {
         velocityY = -6
     }
 
     if (gameOver) {
-        plane.y = planeY;
-        pipeArray = [];
-        score = 0;
-        gameOver = false;
+        towersArray = []
+        score = 0
+        gravity = 0.4
+        gameOver = false
+        plane.y = planeY
+        spanScore.textContent = score
+        gameOverSpan.style.display = 'none'
+        explosion.style.display = 'none'
+        explosionSound.pause()
+        explosionSound.currentTime = 0
+        explosionSoundPeople.pause()
+        explosionSoundPeople.currentTime = 0
     }
 }
 
